ec9f29bd3bc9130bae0bc08faae7c35a472c51f4
sanjeev <sanjeev@gmail.com>
2017-02-14 00:00:19 +0530

    #7 Added models fielname in model file

--- cart/cartapp/models.py
+++ cart/cartapp/models.py
@@ -2,4 +2,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
+class RegisterUsers(models.Model):
+    firstnanme = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200, null=True, blank=True, unique=True)
+    password = models.CharField(max_length=200)
+    Address = models.CharField(max_length=400)
+    contact = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = "registered_users"
 # Create your models here.
