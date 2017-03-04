1757e64340f2de19e17ef5728418e51c295f2e38
vitu03 <imvirtu3@gmail.com>
2017-03-05 02:25:54 +0530

    #13 Added Registration logic in django and sync with databse.

--- cart/cartapp/models.py
+++ cart/cartapp/models.py
@@ -4,11 +4,11 @@ from django.db import models
 
 
 class RegisterUsers(models.Model):
-    firstnanme = models.CharField(max_length=200)
+    firstname = models.CharField(max_length=200)
     lastname = models.CharField(max_length=200)
     email = models.EmailField(max_length=200, null=True, blank=True, unique=True)
     password = models.CharField(max_length=200)
-    Address = models.CharField(max_length=400)
+    address = models.CharField(max_length=400)
     contact = models.CharField(max_length=200)
     city = models.CharField(max_length=200)
     zipcode = models.CharField(max_length=200)
--- cart/cartapp/views.py
+++ cart/cartapp/views.py
@@ -6,12 +6,48 @@ from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
+from cartapp.models import RegisterUsers
 
 
 def index(request):
     return render_to_response("index.html", context_instance = RequestContext(request))
 
 def register(request):
+    if request.method == "POST":
+        firstname = request.POST['first_name']
+        print firstname
+        lastname = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        address = request.POST['address']
+        phone = request.POST['phone']
+        city = request.POST['city']
+        zipcode = request.POST['zipcode']
+        country = request.POST['country']
+        print firstname, lastname,email,password,address,phone,city,zipcode,country
+        # password = hashlib.md5(pword).hexdigest()
+        # print(password)
+        registration = RegisterUsers(
+            firstname = firstname,
+            lastname =lastname,
+            # password = md5_crypt.encrypt(pword),
+            # password = hashlib.md5(pword),
+            # password = hashlib.new(pword).hexdigest(),
+            email= email,
+            password= password,
+            address = address,
+            contact = phone,
+            city = city,
+            zipcode = zipcode,
+            country = country
+
+        )
+        registration.save()
+        return render_to_response("register.html", {'mess': 'Registration successfully Completed', 'status': 'True'},
+                                      context_instance=RequestContext(request))
+    else:
+        return render_to_response("register.html", context_instance=RequestContext(request))
+
     return render_to_response("register.html", context_instance = RequestContext(request))
 
 def contact(request):
@@ -23,43 +59,43 @@ def collection(request):
 def aboutUs(request):
     return render_to_response("about-us.html", context_instance=RequestContext(request))
 
-@csrf_protect
-def register(request):
-    if request.method == 'POST':
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            user = User.objects.create_user(
-                username=form.cleaned_data['username'],
-                password=form.cleaned_data['password1'],
-                email=form.cleaned_data['email']
-            )
-            return HttpResponseRedirect('/register/success/')
-    else:
-        form = RegistrationForm()
-    variables = RequestContext(request, {
-        'form': form
-    })
-
-    return render_to_response(
-        'registration/register.html',
-        variables,
-    )
-
-
-def register_success(request):
-    return render_to_response(
-        'registration/success.html',
-    )
-
-
-def logout_page(request):
-    logout(request)
-    return HttpResponseRedirect('/')
-
-
-@login_required
-def home(request):
-    return render_to_response(
-        'home.html',
-        {'user': request.user}
-    )
+# @csrf_protect
+# def register(request):
+#     if request.method == 'POST':
+#         form = RegistrationForm(request.POST)
+#         if form.is_valid():
+#             user = User.objects.create_user(
+#                 username=form.cleaned_data['username'],
+#                 password=form.cleaned_data['password1'],
+#                 email=form.cleaned_data['email']
+#             )
+#             return HttpResponseRedirect('/register/success/')
+#     else:
+#         form = RegistrationForm()
+#     variables = RequestContext(request, {
+#         'form': form
+#     })
+#
+#     return render_to_response(
+#         'registration/register.html',
+#         variables,
+#     )
+#
+#
+# def register_success(request):
+#     return render_to_response(
+#         'registration/success.html',
+#     )
+#
+#
+# def logout_page(request):
+#     logout(request)
+#     return HttpResponseRedirect('/')
+#
+#
+# @login_required
+# def home(request):
+#     return render_to_response(
+#         'home.html',
+#         {'user': request.user}
+#     )
